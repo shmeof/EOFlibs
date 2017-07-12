310319cf0dfb3f35a329f6b2648658f7d0c2fc6e
eof <eof000@qq.com>
2017-07-12 16:44:39 +0800

    add code

---
+++ app/src/main/java/com/eof/libs/eoflibs/MainActivity.java
@@ -0,0 +1,13 @@
+package com.eof.libs.eoflibs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
